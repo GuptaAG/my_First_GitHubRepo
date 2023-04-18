75c1addcc46b25717c469de3f3cf3c038b0edf31
GuptaAG <131199227+GuptaAG@users.noreply.github.com>
2023-04-18 15:59:56 -0400

    Update StreamIt_App.py

--- StreamIt_App.py
+++ StreamIt_App.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('Good, Mornig dear')
 streamlit.header('Ready Fast for BreakFast !!')
-straemlit.text('Manue')
-strealit.text('South Indian')
+streamlit.text('Manue')
+streamlit.text('South Indian')
