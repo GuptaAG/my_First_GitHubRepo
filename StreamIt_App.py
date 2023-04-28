d1f204685f59e7fc6d7c04e0a21f01cccd1cb399
GuptaAG <131199227+GuptaAG@users.noreply.github.com>
2023-04-28 09:20:22 -0400

    Update StreamIt_App.py

--- StreamIt_App.py
+++ StreamIt_App.py
@@ -18,14 +18,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-my_fruit_list1 = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list1 = my_fruit_list1.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected1=streamlit.multiselect("Pick some fruits from list:", list(my_fruit_list1.index),['mango','banana'])
-fruits_to_show1 = my_fruit_list1.loc[fruits_selected]
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show1)
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
